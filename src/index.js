887f19bcf904f6a335165acfaa9995d263d08a5b
M-Tyshchenko <m.tish2406@gmail.com>
2023-09-30 22:04:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-
 import { App } from 'components/App';
 import { persistor, store } from 'redux/store';
 import { BrowserRouter } from 'react-router-dom';
