79573516fa5b7fd5fab93067d9e6fa178f86dbeb
M-Tyshchenko <122963239+M-Tyshchenko@users.noreply.github.com>
2023-09-30 19:04:15 +0000

    Deploying to gh-pages from @ M-Tyshchenko/goit-react-hw-08-phonebook@330b675a1ddd5d71b9016a436b370df0da6a891c 🚀

---
+++ static/js/291.21c14fd0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[291],{2291:function(e,n,r){r.r(n),r.d(n,{default:function(){return i}});var a,t=r(168),o=r(5867).ZP.h1(a||(a=(0,t.Z)(["\n  margin: 0 auto;\n"]))),c=r(184),i=function(){return(0,c.jsxs)(o,{children:["Phonebook manager welcome page"," ",(0,c.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})}}}]);
+//# sourceMappingURL=291.21c14fd0.chunk.js.map
